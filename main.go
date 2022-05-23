27423c5fba02d3a625c0f46f154f6c0fbedb1385
Anton Obretenov <obretenova@vmware.com>
2022-05-23 04:30:32 -0700

    added transactions as a separate module

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"CardsAndTransactions/transactions"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	// Server supports only a single client as there is no budget :D
+	// Nor part of the assignment
+	client := transactions.Client{}
+	println(client)
+
+	http.HandleFunc("/add-card", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
